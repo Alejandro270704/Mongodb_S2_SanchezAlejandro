a6bb086907974674b7ed386f83255718bb560deb
Alejandro <yasdmi048@gmail.com>
2025-10-09 10:25:16 -0500

    feat :sparkles: update

--- Dia4/script.js
+++ Dia4/script.js
@@ -1,44 +1,44 @@
 use incautaciones;
 // ¿Cuántos municipios comienzan con "La" y cuál es la cantidad total incautada en ellos?
 db.incautacionMarihuana.aggregate([
-    {
-        $lookup:{
-            from:"Municipio",
-            localField:"idMunicipio",
-            foreignField:"_id",
-            as: "Municipio"
-        }
-    },
-    { $unwind: "$Municipio" },
-    {
-        $lookup:{
-            from:"incautacion",
-            localField:"idIncautacion",
-            foreignField:"_id",
-            as: "incautacion"
-        }
-    },
-    { $unwind: "$incautacion" },
-    {
-        $match:{
-            "Municipio.nombreMunicipio":{$regex: /^La/i }
+  {
+    $lookup: {
+      from: "Municipio",
+      localField: "idMunicipio",
+      foreignField: "_id",
+      as: "Municipio"
+    }
+  },
+  { $unwind: "$Municipio" },
+  {
+    $lookup: {
+      from: "incautacion",
+      localField: "idIncautacion",
+      foreignField: "_id",
+      as: "incautacion"
+    }
+  },
+  { $unwind: "$incautacion" },
+  {
+    $match: {
+      "Municipio.nombreMunicipio": { $regex: /^La/i }
 
-        }  
-    },
-    {
-        $group:{
-            _id:"$Municipio.nombreMunicipio",
-            total:{$sum:"$incautacion.Cantidad"}
-        }
-    },
-    {
-        $project:{
-            _id:0,
-            nombreMunicipio:"$_id",
-            total:1
-        }
-    },
-    {
+    }
+  },
+  {
+    $group: {
+      _id: "$Municipio.nombreMunicipio",
+      total: { $sum: "$incautacion.Cantidad" }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      nombreMunicipio: "$_id",
+      total: 1
+    }
+  },
+  {
     $limit: 5
   }
 ])
@@ -110,40 +110,127 @@ db.incautacionMarihuana.aggregate([
 
 //Por cada año, muestra los 3 municipios con más incautaciones, pero únicamente si su nombre contiene la letra "z".
 db.incautacionMarihuana.aggregate([
-    {
-        $lookup:{
-            from:"Municipio",
-            localField:"idMunicipio",
-            foreignField:"_id",
-            as:"Municipio"
-        }
-    },
+  {
+    $lookup: {
+      from: "Municipio",
+      localField: "idMunicipio",
+      foreignField: "_id",
+      as: "Municipio"
+    }
+  },
   { $unwind: "$Municipio" },
-    {
-        $lookup: {
+  {
+    $lookup: {
       from: "incautacion",
       localField: "idIncautacion",
       foreignField: "_id",
       as: "incautacion"
     }
-    },
+  },
   { $unwind: "$incautacion" },
   {
-    $match:{
-        "Municipio.nombreMunicipio":{$regex:/z/i}
+    $match: {
+      "Municipio.nombreMunicipio": { $regex: /z/i }
     }
   },
   {
-    $addFields:{
-        anio:{$year:"$incautacion.fecha"}
+    $addFields: {
+      anio: { $year: "$incautacion.fecha" }
     }
   },
-   {
+  {
     $group: {
       _id: { anio: "$anio", municipio: "$Municipio.nombreMunicipio" },
       totalIncautaciones: { $sum: 1 }
     }
   },
-   
+  {
+    $limit: 3
+  },
+   {
+    $project: {
+      _id: 0,
+      anio: "$_id.anio",
+      municipio: "$_id.municipio",
+      totalIncautaciones: 1
+    }
+  }
 
 ])
+//¿Qué unidad de medida aparece en registros de municipios que empiecen por "Santa"?
+db.incautacionMarihuana.aggregate([
+  {
+    $lookup: {
+      from: "Municipio",
+      localField: "idMunicipio",
+      foreignField: "_id",
+      as: "Municipio"
+    }
+  },
+  { $unwind: "$Municipio" },
+  {
+    $match: {
+      "Municipio.nombreMunicipio": { $regex: /^Santa/i }
+    }
+  },
+  {
+    $lookup: {
+      from: "Unidad",
+      localField: "idUnidad",
+      foreignField: "_id",
+      as: "Unidad"
+    }
+  },
+  { $unwind: "$Unidad" },
+  {
+    $project: {
+      _id: 0,
+      Municipio: "$Municipio.nombreMunicipio",
+      Unidad: "$Unidad.Unidad"
+    }
+  }
+])
+//¿Cuál es la cantidad promedio de incautaciones en los municipios cuyo nombre contiene "Valle"?
+db.incautacionMarihuana.aggregate([
+  {
+    $lookup: {
+      from: "Municipio",
+      localField: "idMunicipio",
+      foreignField: "_id",
+      as: "Municipio"
+    }
+  },
+  { $unwind: "$Municipio" },
+  {
+    $match:{
+      "Municipio.nombreMunicipio":{$regex:/Valle/i}
+    }
+  },
+  {
+    $lookup:{
+      from:"incautacion",
+      localField:"idIncautacion",
+      foreignField:"_id",
+      as:"incautacion"
+    }
+  },
+  { $unwind: "$incautacion" },
+  {
+    $group:{
+      _id:"$Municipio.nombreMunicipio",
+      promedioCantidad:{$avg: "$incautacion.Cantidad"}
+    }
+  },
+  {
+    $project:{
+       _id: 0,
+      Municipio: "$_id",
+      promedioCantidad: 1
+    }
+  },
+  {
+    $limit:5
+  }
+]);
+
+
